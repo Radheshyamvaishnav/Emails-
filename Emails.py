ac631df0eec790261d3391962ad29ae8ed12fa7b
Radheshyam Vaishnav <70651912+Radheshyamvaishnav@users.noreply.github.com>
2020-09-17 21:45:36 +0530

    Updated Emails.py

--- Emails.py
+++ Emails.py
@@ -2,20 +2,18 @@
 import smtplib
 
 
-for i in range(10):
-    sender_email = "Enter Sender's Email Here"
-    rec_email = "Enter Recevier's Email Here"
-    password = "Enter Sender's Password Here"  # It's always a best practice to use App Password over here which is generated after enabling 2 step verification in your google account.
-    msg = ""
-    server = smtplib.SMTP('smtp.gmail.com', 587)
+sender_email = "Enter Sender's Email Here"
+rec_email = "Enter Recevier's Email Here"
+password = "Enter Sender's Password Here"  # It's always a best practice to use App Password over here which is generated after enabling 2 step verification in your google account.
+msg = ""   # Enter the Message you want to Send  
+server = smtplib.SMTP('smtp.gmail.com', 587)
 
-    server.starttls()
+server.starttls()
 
-    server.login(sender_email, password)
-    server.sendmail(sender_email, rec_email, msg)
-
-    print("emailSent",i)
+server.login(sender_email, password)
+server.sendmail(sender_email, rec_email, msg)
 
 
-#Simple12345
+print("EmailSent")
+
 
